2b07351a94d27bc1f59153496573249aeb51010b
Bruno <fc49498@alunos.fc.ul.pt>
2019-10-21 01:12:21 +0100

    falta butoes no user, TREE OP xD

---
+++ app/src/main/java/com/example/gogreen/AvatarFragment.java
@@ -0,0 +1,33 @@
+package com.example.gogreen;
+
+
+import android.media.Image;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AvatarFragment extends Fragment {
+
+
+    public AvatarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_avatar, container, false);
+    }
+
+}
