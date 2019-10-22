4c5961bfe4335d448900a063af716b1b4651131d
Bruno <fc49498@alunos.fc.ul.pt>
2019-10-22 23:53:34 +0100

    RIP SSS

--- app/src/main/java/com/example/gogreen/CostumizeAvatarActivity.java
+++ app/src/main/java/com/example/gogreen/CostumizeAvatarActivity.java
@@ -17,25 +17,35 @@ import android.widget.ImageView;
 public class CostumizeAvatarActivity extends AppCompatActivity  {
     ImageView image;
     Drawable chosen;
+    ColorMatrix matrix;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_costumize_avatar);
 
         ImageView imageview = findViewById(R.id.frame1);
-        ColorMatrix matrix = new ColorMatrix();
+        matrix = new ColorMatrix();
         matrix.setSaturation(0);
         imageview.setColorFilter(new ColorMatrixColorFilter(matrix));
+
+
+
     }
 
 
 
     public void changeAvatar(View view){
+
+        ColorMatrix matrix1 = new ColorMatrix();
+        matrix1.setSaturation(0);
         ImageView avatar = findViewById(R.id.avatar);
         image =  findViewById(view.getId());
 
         chosen = image.getDrawable();
 
+
+
+        if(image.getColorFilter() == null)
         avatar.setImageDrawable(chosen);
 
         }
