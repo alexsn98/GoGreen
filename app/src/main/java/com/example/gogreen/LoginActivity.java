6572dac2ccdebe1f3dd37767997427f022bc76a4
alexNasc <alexmento98@gmail.com>
2019-11-18 14:57:27 +0000

    login usando google

--- app/src/main/java/com/example/gogreen/LoginActivity.java
+++ app/src/main/java/com/example/gogreen/LoginActivity.java
@@ -5,16 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1;
     EditText usernameField;
-    EditText passwordField ;
-    Button loginButton ;
+    EditText passwordField;
+    Button loginButton;
+    GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +34,57 @@ public class LoginActivity extends AppCompatActivity {
         passwordField = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
         getSupportActionBar().hide(); //esconder app bar
+
+        //login auth google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                signIn(v);
+            }
+        });
+
+        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void signIn(View v) {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } catch (ApiException e) {
+            Log.w("pau", "signInResult:failed code=" + e.getMessage());
+            //updateUI(null);
+        }
     }
 
     public void login(View v) {
         if (!validate()) {
-
             return;
         }
 
